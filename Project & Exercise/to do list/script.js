5f6c05f69d6cf6e89aabf8c2c74fbd594f650a19
Kourosh Rahimi <kourosh2004rahimi@gmail.com>
2025-03-28 19:12:20 +0330

    Add files via upload

---
+++ Project & Exercise/to do list/script.js
@@ -0,0 +1,29 @@
+document.getElementById("addTaskBtn").addEventListener("click", function () {
+  const taskInput = document.getElementById("taskInput");
+  const taskText = taskInput.value.trim();
+
+  if (taskText !== "") {
+    addTask(taskText);
+    taskInput.value = "";
+  }
+});
+
+function addTask(taskText) {
+  const taskList = document.getElementById("taskList");
+
+  const li = document.createElement("li");
+  li.textContent = taskText;
+
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "حذف";
+  deleteButton.addEventListener("click", function () {
+    taskList.removeChild(li);
+  });
+
+  li.addEventListener("click", function () {
+    li.classList.toggle("completed");
+  });
+
+  li.appendChild(deleteButton);
+  taskList.appendChild(li);
+}
